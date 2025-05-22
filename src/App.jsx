c8b2c36d11e04b9ca88722b279f23847f0c5062d
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-05-22 12:54:14 +0900

    [feat & style] 투두리스트 수정 input 숨기기 기능 추가 및 전체 스타일 변경
    - 수정 버튼을 클릭할 때마다 visible 상태값이 변하면서 input을 보여주거나
    숨김
    - input의 value가 빈 문자열인 상태에서 수정을 완료하면 값을 수정하지
      않고 input만 숨김
    - 수정 input을 열 때마다 inputValue 초기화
    - Todo 앱의 제목을 보여주는 헤더 추가
    - flex를 이용해서 전체 컨테이너 화면 가운데 정렬
    - li 요소의 자식 요소의 변화에 따라 width 동적으로 변하도록 스타일 변경
    - input, button 등에 색상 및 hover 효과 추가

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,21 @@ function App() {
   ]);
 
   return (
-    <>
-      <TodoList todoList={todoList} setTodoList={setTodoList} />
-      <hr />
-      <TodoInput todoList={todoList} setTodoList={setTodoList} />
-    </>
+    <div className="todo-container">
+      <TodoHeader />
+      <div className="todo-list-input">
+        <TodoList todoList={todoList} setTodoList={setTodoList} />
+        <TodoInput todoList={todoList} setTodoList={setTodoList} />
+      </div>
+    </div>
+  );
+}
+
+function TodoHeader() {
+  return (
+    <header className="todo-header">
+      <h2>TODO LIST</h2>
+    </header>
   );
 }
 
@@ -21,7 +31,7 @@ function TodoInput({ todoList, setTodoList }) {
   const [inputValue, setInputValue] = useState("");
 
   return (
-    <>
+    <div className="todo-input">
       <input
         value={inputValue}
         onChange={(event) => setInputValue(event.target.value)}
@@ -32,11 +42,10 @@ function TodoInput({ todoList, setTodoList }) {
           const newTodoList = [...todoList, newTodo];
           setTodoList(newTodoList);
           setInputValue("");
-        }}
-      >
+        }}>
         추가하기
       </button>
-    </>
+    </div>
   );
 }
 
@@ -52,33 +61,47 @@ function TodoList({ todoList, setTodoList }) {
 
 function Todo({ todo, setTodoList }) {
   const [inputValue, setInputValue] = useState("");
+  const [visible, setVisible] = useState(false);
   return (
     <li>
       {todo.content}
       <input
+        style={{ display: visible ? "block" : "none" }}
         value={inputValue}
         onChange={(event) => setInputValue(event.target.value)}
       />
-      <button
-        onClick={() => {
-          setTodoList((prev) =>
-            prev.map((el) =>
-              el.id === todo.id ? { ...el, content: inputValue } : el
-            )
-          );
-        }}
-      >
-        수정
-      </button>
-      <button
-        onClick={() => {
-          setTodoList((prev) => {
-            return prev.filter((el) => el.id !== todo.id);
-          });
-        }}
-      >
-        삭제
-      </button>
+      <div className="buttons">
+        <button
+          className="edit-btn"
+          onClick={() => {
+            // 수정 버튼을 클릭하면 visible 상태값 변경
+            setVisible((prev) => !prev);
+
+            // input이 보여지는 상태라면
+            if (visible) {
+              // (input의 값이 빈 문자열일 때는 값을 변경하지 않고 끝냄)
+              if (inputValue === "") return;
+              // 선택된 투두의 content 값을 input에 입력한 값으로 변경
+              setTodoList((prev) =>
+                prev.map((el) =>
+                  el.id === todo.id ? { ...el, content: inputValue } : el
+                )
+              );
+              setInputValue("");
+            }
+          }}>
+          수정
+        </button>
+        <button
+          className="delete-btn"
+          onClick={() => {
+            setTodoList((prev) => {
+              return prev.filter((el) => el.id !== todo.id);
+            });
+          }}>
+          삭제
+        </button>
+      </div>
     </li>
   );
 }
